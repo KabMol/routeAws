f2a01afb9482f0da092b8bb31a556347c3ec5c5a
Kabo Molete <kabomolete@gmail.com>
2024-04-20 21:10:20 +0200

    aws test home change

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
       <p id="zero-state">
-        This is a demo for React Router change.
+        This is a demo for React Router change.123
         <br />
         Check out{" "}
         <a href="https://reactrouter.com">
