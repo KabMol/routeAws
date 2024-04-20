e37269e4326a4b6eb1c28f9400000686b6055a9a
Kabo Molete <kabomolete@gmail.com>
2024-04-20 18:19:06 +0200

    change the index

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,11 @@
 export default function Index() {
     return (
       <p id="zero-state">
-        This is a demo for React Router.
+        This is a demo for React Router change.
         <br />
         Check out{" "}
         <a href="https://reactrouter.com">
-          the docs at reactrouter.com
+          the docs at reactrouter.com 
         </a>
         .
       </p>
